b0d465119507d1c98f5304c91cc100e346cd298a
Leonardo <yleeoembd@gmail.com>
2022-11-07 10:06:06 -0300

    Feat: Retrieve update address user with permissions

--- addresses/urls.py
+++ addresses/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("adress/", views.AdressListCreate.as_view()),
     path("adress/<pk>/", views.AdressDetail.as_view()),
 ]
--- addresses/views.py
+++ addresses/views.py
@@ -4,20 +4,24 @@ from rest_framework.views import Request, Response, status, APIView
 from .serializers import Address, SerializerAddress
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from .permissions import IsOwnerOrReadOnly
 
 
-class AdressListCreate(generics.ListCreateAPIView):
+# class AdressListCreate(generics.ListCreateAPIView):
+#     authentication_classes = [TokenAuthentication]
+#     permission_classes = [IsAuthenticated]
+
+#     queryset = Address.objects.all()
+#     serializer_class = SerializerAddress
+
+#     def perform_create(self, serializer):
+#         user = self.request.user
+#         serializer.save(user=user)
+
+
+class AdressDetail(generics.RetrieveUpdateAPIView):
     authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsOwnerOrReadOnly]
 
-    queryset = Address.objects.all()
-    serializer_class = SerializerAddress
-
-    def perform_create(self, serializer):
-        user = self.request.user
-        serializer.save(user=user)
-
-
-class AdressDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Address.objects
     serializer_class = SerializerAddress
