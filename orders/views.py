65e07802fff36b22916f12fef110f58282f2590c
matheus-bomfim <mtsbomfim2002@gmail.com>
2022-11-08 16:17:58 -0300

    tryfix: frete

--- orders/views.py
+++ orders/views.py
@@ -8,7 +8,9 @@ from books.models import Book
 from rest_framework import status
 from django.forms.models import model_to_dict
 from rest_framework.response import Response
+from pycorreios.pycorreios import Correios
 import ipdb
+from users.models import User
 
 
 class OrderListAndCreateViews(generics.ListCreateAPIView):
@@ -25,11 +27,30 @@ class OrderListAndCreateViews(generics.ListCreateAPIView):
         return super().get_queryset()
     """
     def create(self, request, *args, **kwargs):
-        # ipdb.set_trace()
-        # print("1" * 1)
+        ipdb.set_trace()
         totalValue = 0
         ammount = 0
         listOrders = []
+        user = User.objects.get(id=request.user.id)
+        fields = {
+          "cod": Correios.PAC, 
+          "GOCEP": "02912000",
+          "HERECEP": user.address.zip_code,
+          "peso": "1",
+          "formato": "1", # caixa/pacote
+          "comprimento": "18",
+          "altura": "9",
+          "largura": "13.5",
+          "diametro": "0"
+          }
+        test = Correios.frete(Correios.PAC,"02912000",user.address.zip_code,1,1,18,9,13.5,0,1)
+        if test['Erro'] != '0':
+            print ('Deu erro! :(')
+            print (test['Erro'])
+            print (test['MsgErro'])
+        else:
+            print("Valor: R$%s\nPrazo de Entrega: %s" % (test['Valor'],test['PrazoEntrega']))
+
         for elem in request.data:
             object_book = get_object_or_404(Book, id=elem["books"])
             totalValue += object_book.price * elem["ammount_items"]
