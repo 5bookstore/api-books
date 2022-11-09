8f2d1fa5b9df213e46118e3c3ad103eab40f3d75
matheus-bomfim <mtsbomfim2002@gmail.com>
2022-11-09 15:12:06 -0300

    tryFix Frete

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 import uuid
-
+from correios_utils import (
+    Cotacao,
+    FormatoEncomenda,
+    SimNao,
+    Servico,
+    realizar_cotacao,
+    get_descricao_servico,
+)
+from users.serializers import UserSerializer
+from books.serializers import BookSerializer
 
 class Status_Order(models.TextChoices):
     ORDER_CREATED = "Order created"
@@ -26,3 +35,30 @@ class Order(models.Model):
     user = models.ForeignKey(
         "users.User", on_delete=models.CASCADE, related_name="orders"
     )
+
+    def frete_value(self):
+        totalValue = 0
+        ammount = 0
+        user = UserSerializer(self.user)
+        zipCode = user.data["address"]["zip_code"]
+        book = self.books
+        print(book)
+        frete = realizar_cotacao(
+            cep_origem= "70002900",
+            cep_destino= zipCode,
+            codigos_servicos= [Servico.PAC, Servico.SEDEX, Servico.SEDEX_10],
+            peso= book.weigth,
+            comprimento= book.width,
+            altura= book.length,
+            largura= book.width,
+            diametro= book.diameter,
+            formato_encomenda= FormatoEncomenda.CAIXA_PACOTE,
+            valor_declarado= book.price,
+            mao_propria= SimNao.NAO,
+            aviso_recebimento= SimNao.NAO,
+            codigo_empresa= "08082650",
+            senha_empresa= "564321",
+            )
+        totalValue += book.price * self.ammount_items
+        ammount = self.ammount_items
+        return frete[0].valor
--- orders/views.py
+++ orders/views.py
@@ -3,21 +3,11 @@ from .models import Order
 from .serializers import OrderSerializer
 from rest_framework.authentication import TokenAuthentication
 from .permissions import isUserOrAdmin
+from rest_framework.response import Response
+import ipdb
 from django.shortcuts import get_object_or_404
 from books.models import Book
-from rest_framework import status
-from django.forms.models import model_to_dict
-from rest_framework.response import Response
-from users.models import User
-import ipdb
-from correios_utils import (
-    Cotacao,
-    FormatoEncomenda,
-    SimNao,
-    Servico,
-    realizar_cotacao,
-    get_descricao_servico,
-)
+
 
 
 class OrderListAndCreateViews(generics.ListCreateAPIView):
@@ -33,43 +23,12 @@ class OrderListAndCreateViews(generics.ListCreateAPIView):
     def get_queryset(self):
         return super().get_queryset()
     """
+
     def create(self, request, *args, **kwargs):
-        # ipdb.set_trace()
-        # print("1" * 1)
-        totalValue = 0
-        ammount = 0
-        listOrders = []
-        user = User.objects.get(id=request.user.id)
-        address = model_to_dict(user.address)
         for elem in request.data:
-            object_book = get_object_or_404(Book, id=elem["books"])
-            frete = realizar_cotacao(
-                cep_origem="70002900",
-                cep_destino=address["zip_code"],
-                codigos_servicos=[Servico.PAC, Servico.SEDEX, Servico.SEDEX_10],
-                peso=object_book.weigth,
-                comprimento=object_book.width,
-                altura=object_book.length,
-                largura=object_book.width,
-                diametro=object_book.diameter,
-                formato_encomenda=FormatoEncomenda.CAIXA_PACOTE,
-                valor_declarado=object_book.price,
-                mao_propria=SimNao.NAO,
-                aviso_recebimento=SimNao.NAO,
-                codigo_empresa="08082650",
-                senha_empresa="564321",
-            )
-            totalValue += object_book.price * elem["ammount_items"]
-            ammount = elem["ammount_items"]
-            data = {
-                "user":request.user,
-                "shipping":frete[0].valor,
-                "ammount_items":ammount,
-                "total_value":totalValue
-            }
-            create = Order.objects.create(**data)
-            create.books.add(object_book)
-            listOrders.append(model_to_dict(create))
-        return Response(
-            listOrders, status=status.HTTP_201_CREATED
-        )
+            object_book = Book.objects.get(id=elem["books"])
+            serializer = OrderSerializer(data=elem,context={"user":request.user})
+            serializer.is_valid(raise_exception=True)
+            serializer.save(books=object_book)
+        return Response(serializer.data)
+
